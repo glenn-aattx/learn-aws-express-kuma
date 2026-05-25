71a9b0aaeaa78c49bc3242dc5774a1bdf40363ea
glenn-aattx <glenn@aattx.com>
2026-05-25 16:46:26 +0900

    Add Sequelize board endpoint

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+const port = 80;
+require('dotenv').config()
+const { Sequelize, DataTypes } = require('sequelize');
 
-const app = express();
-const port = process.env.PORT || 80;
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: 'mysql'
+  }
+);
 
-app.get('/', (req, res) => {
-  res.send('Hello, kuma!');
-});
+const Board = sequelize.define('boards', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  title: {
+    type: DataTypes.STRING
+  },
+  content: {
+    type: DataTypes.STRING
+  }
+}, {
+  timestamps: false
+})
 
 app.get('/health', (req, res) => {
-  res.sendStatus(200);
+  res.status(200).send("Success Health Check");
+})
+
+app.get('/boards', async (req, res) => {
+  await Board.create({
+    title: "게시글 제목",
+    content: "게시글 내용"
+  });
+
+  const boards = await Board.findAll();
+
+  res.status(200).send(boards);
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+app.listen(port, async () => {
+  await sequelize.authenticate();
+  await sequelize.sync({ force: true });
+  console.log(`Example app listening on port ${port}`)
+})
